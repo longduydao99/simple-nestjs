86d9e82f9682e6a5ea182a1cbbe5fe98b3966b99
Long Dao <long.dao@zodinet.com>
2023-03-01 22:01:33 +0700

    chore: add validator pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -9,6 +10,16 @@ async function bootstrap() {
   const config = new DocumentBuilder().setTitle('Nest app').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      validationError: {
+        target: false,
+      },
+    }),
+  );
 
   await app.listen(3000);
 }
